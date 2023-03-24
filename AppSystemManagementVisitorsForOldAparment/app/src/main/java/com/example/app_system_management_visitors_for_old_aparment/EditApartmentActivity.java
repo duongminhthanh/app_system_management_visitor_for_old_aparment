7874e559b0248777fd67572099a79df81a738fe5
duongminhthanh <duongminhthanh1999td@gmail.com>
2023-03-24 08:46:09 +0700

    Update project

---
+++ AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/EditApartmentActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_system_management_visitors_for_old_aparment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditApartmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_apartment);
+    }
+}
