e4df4a8f6b2fec3239cd903ee10563aa85994e8a
duongminhthanh <duongminhthanh1999td@gmail.com>
2023-03-30 10:48:26 +0700

    update list screens

---
+++ AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/ListVisitorActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_system_management_visitors_for_old_aparment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListVisitorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_visitor);
+    }
+}
---
+++ AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/ManageListAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_system_management_visitors_for_old_aparment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageListAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_list_account);
+    }
+}
