5ad9de2b44ab86c5e8e321671de573e0e94a657f
duongminhthanh <duongminhthanh1999td@gmail.com>
2023-03-31 17:40:29 +0700

    update project

--- AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/ManageListApartmentActivity.java
+++ AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/ManageListApartmentActivity.java
@@ -1,16 +1,14 @@
 package com.example.app_system_management_visitors_for_old_aparment;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DashboardActivity extends AppCompatActivity {
+import android.os.Bundle;
 
+public class ManageListApartmentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-
+        setContentView(R.layout.activity_manage_list_apartment);
     }
 }
--- AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/PinCodeActivity.java
+++ AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/PinCodeActivity.java
@@ -1,14 +1,51 @@
 package com.example.app_system_management_visitors_for_old_aparment;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class PinCodeActivity extends AppCompatActivity {
+
+    EditText ed1, ed2, ed3, ed4;
+    String pin_code="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pin_code);
+        ed1 = findViewById(R.id.ed_1);
+        ed2 = findViewById(R.id.ed_2);
+        ed3 = findViewById(R.id.ed_3);
+        ed4 = findViewById(R.id.ed_4);
+
+        ed1.setOnClickListener(view -> {
+            if (ed1.getText().toString().isEmpty()){
+                ed1.setText(ed1.getText().toString());
+                pin_code= ed1.getText().toString();
+            }
+        });
+
+        ed2.setOnClickListener(view -> {
+            if (ed2.getText().toString().isEmpty()){
+                ed2.setText(ed2.getText().toString());
+                pin_code+= ed2.getText().toString();
+            }
+        });
+
+        ed3.setOnClickListener(view -> {
+            if (ed3.getText().toString().isEmpty()){
+                ed3.setText(ed3.getText().toString());
+                pin_code+=ed3.getText().toString();
+            }
+        });
+        ed4.setOnClickListener(view -> {
+            if (ed4.getText().toString().isEmpty()){
+                ed4.setText(ed4.getText().toString());
+                pin_code+= ed4.getText().toString();
+            }
+        });
+        Log.println(Log.INFO,"pin_code", pin_code);
     }
 }
