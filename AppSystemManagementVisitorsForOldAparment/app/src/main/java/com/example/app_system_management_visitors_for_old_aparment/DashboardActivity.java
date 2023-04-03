2b2eaa3eb4d8e406491e2e5300af129484408c49
duongminhthanh <duongminhthanh1999td@gmail.com>
2023-04-03 10:02:47 +0700

    update code DashboardActivity

--- AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/DashboardActivity.java
+++ AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/DashboardActivity.java
@@ -1,14 +1,12 @@
 package com.example.app_system_management_visitors_for_old_aparment;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
+public class DashboardActivity extends AppCompatActivity  {
 
     ImageView imgVisitor,imgApartment,imgFeedbacks,imgLogout;
 
@@ -20,32 +18,27 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
     }
     public void createView(){
         imgApartment=findViewById(R.id.image_apartment);
-        imgApartment.setOnClickListener(this);
+        imgApartment.setOnClickListener(view -> {
+            Intent listApartment=new Intent(this,ListApartmentActivity.class);
+            startActivity(listApartment);
+        });
         imgFeedbacks=findViewById(R.id.image_feedbacks);
-        imgFeedbacks.setOnClickListener(this);
+        imgFeedbacks.setOnClickListener(view -> {
+            Intent listFeedbacks=new Intent(this,ListFeedbackActivity.class);
+            startActivity(listFeedbacks);
+        });
         imgLogout=findViewById(R.id.image_logout);
-        imgLogout.setOnClickListener(this);
+        imgLogout.setOnClickListener(view -> {
+            Intent login=new Intent(this,LoginActivity.class);
+            startActivity(login);
+        });
         imgVisitor=findViewById(R.id.image_visitor);
-        imgVisitor.setOnClickListener(this);
+        imgVisitor.setOnClickListener(view -> {
+            Intent listVisitor=new Intent(this,ListVisitorActivity.class);
+            startActivity(listVisitor);
+        });
 
     }
 
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.image_visitor:
-                Intent listVisitor=new Intent(this,ListVisitorActivity.class);
-                startActivity(listVisitor);
-            case R.id.image_apartment:
-                Intent listApartment=new Intent(this,ListApartmentActivity.class);
-                startActivity(listApartment);
-            case R.id.image_feedbacks:
-                Intent listFeedbacks=new Intent(this,ListFeedbackActivity.class);
-                startActivity(listFeedbacks);
-            case R.id.image_logout:
-                Intent login=new Intent(this,LoginActivity.class);
-                startActivity(login);
-        }
-    }
+
 }
