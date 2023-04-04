48077966d1efe3537a2850f7ddf55f91813a76e4
duongminhthanh <duongminhthanh1999td@gmail.com>
2023-04-04 13:26:51 +0700

    update code LoginActivity

--- AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/LoginActivity.java
+++ AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/LoginActivity.java
@@ -2,8 +2,6 @@ package com.example.app_system_management_visitors_for_old_aparment;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -25,8 +23,8 @@ public class LoginActivity extends AppCompatActivity {
     Button btn_login;
 
     DatabaseReference myRef;
-    boolean checkAcc;
-    String name="",pass="";
+
+    String name = "", pass = "", username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,49 +33,46 @@ public class LoginActivity extends AppCompatActivity {
         edit_username = findViewById(R.id.edit_name);
         edit_password = findViewById(R.id.edit_password);
         btn_login = findViewById(R.id.btn_login);
-
         btn_login.setOnClickListener(view -> {
-            edit_username.setText(edit_username.getText().toString().trim());
-            edit_password.setText(edit_password.getText().toString().trim());
+            if (edit_username.getText().toString().isEmpty() &&
+                    edit_password.getText().toString().isEmpty()) {
+                edit_username.setText(edit_username.getText().toString());
+                edit_password.setText(edit_username.getText().toString());
+            }
             name = edit_username.getText().toString();
             pass = edit_password.getText().toString();
             readDataAccount(name, pass);
-            if (checkAcc == true) {
-                Toast.makeText(LoginActivity.this, "Correct username and password"
-                        , Toast.LENGTH_SHORT).show();
-                Log.d("check acc is ", String.valueOf(checkAcc));
-                Intent login = new Intent(this, MainActivity.class);
-                startActivity(login);
-            } else if (checkAcc == false) {
-                Log.d("check acc is ", String.valueOf(checkAcc));
-                Toast.makeText(LoginActivity.this, "Incorrect username and password"
-                        , Toast.LENGTH_SHORT).show();
-            }
         });
     }
 
-    public void readDataAccount(String name,String pass) {
+    public void readDataAccount(String name, String pass) {
 
         myRef = FirebaseDatabase.getInstance().getReference().child("account");
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange( DataSnapshot snapshot) {
-                String username = snapshot.child("username")
-                        .getValue().toString();
-                String password = (snapshot.child("password")
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                username = Objects.requireNonNull(snapshot.child("username")
                         .getValue()).toString();
-                Log.d("true or false username", String.valueOf(name.equals(username)));
-                Log.d("true or false password", String.valueOf(pass.equals(password)));
-                checkAcc= (name.equals(username) && pass.equals(password));
-//                Log.d("Check acc is: ", String.valueOf(checkAcc));
+                password = (Objects.requireNonNull(snapshot.child("password")
+                        .getValue())).toString();
+
+                if (name.equals(username) && pass.equals(password)) {
+                    Toast.makeText(LoginActivity.this, "Correct username and password"
+                            , Toast.LENGTH_SHORT).show();
+                    Intent login = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(login);
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Incorrect username and password"
+                            , Toast.LENGTH_SHORT).show();
+                }
 
             }
+
             @Override
-            public void onCancelled( DatabaseError error) {
+            public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
     }
-
-
 }
