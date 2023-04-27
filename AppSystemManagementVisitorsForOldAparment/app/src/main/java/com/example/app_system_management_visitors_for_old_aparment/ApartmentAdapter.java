d16ebf2831c1eeb61ee91181a8128735ccf13695
duongminhthanh <duongminhthanh1999td@gmail.com>
2023-04-27 21:34:21 +0700

    update project part 5

--- AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/ApartmentAdapter.java
+++ AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/ApartmentAdapter.java
@@ -1,13 +1,20 @@
 package com.example.app_system_management_visitors_for_old_aparment;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -15,7 +22,6 @@ import java.util.ArrayList;
 public class ApartmentAdapter extends RecyclerView.Adapter<ApartmentAdapter.MyViewHolder> {
     Context c;
     ArrayList<Apartment> apartments;
-
     public ApartmentAdapter(Context c, ArrayList<Apartment> apartments) {
         this.c = c;
         this.apartments = apartments;
@@ -41,6 +47,12 @@ public class ApartmentAdapter extends RecyclerView.Adapter<ApartmentAdapter.MyVi
         holder.owner_phone.setText(a.getOwner_phone());
         holder.owner_name.setText(a.getOwner_name());
         holder.room_id.setText(a.getRoom_id());
+        holder.callPhone.setOnClickListener(view -> {
+            String phone=a.getOwner_phone();
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse("tel:" + phone));
+            startActivity(c,intent, Bundle.EMPTY);
+        });
     }
 
     @Override
@@ -50,14 +62,14 @@ public class ApartmentAdapter extends RecyclerView.Adapter<ApartmentAdapter.MyVi
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         TextView owner_phone, owner_name, room_id;
-
+        Button callPhone,callZalo;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             owner_phone = itemView.findViewById(R.id.owner_phone);
             owner_name = itemView.findViewById(R.id.owner_name);
             room_id = itemView.findViewById(R.id.room_id);
-
-
+            callPhone=itemView.findViewById(R.id.call_phone);
+            callZalo=itemView.findViewById(R.id.call_zalo);
         }
     }
 }
--- AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/RatingActivity.java
+++ AppSystemManagementVisitorsForOldAparment/app/src/main/java/com/example/app_system_management_visitors_for_old_aparment/RatingActivity.java
@@ -2,13 +2,44 @@ package com.example.app_system_management_visitors_for_old_aparment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class RatingActivity extends AppCompatActivity {
-
+    Button btnSubmit;
+    RatingBar ratingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rating);
+        btnSubmit=findViewById(R.id.btn_submit);
+        ratingBar=findViewById(R.id.rating_start);
+        ratingBar.setOnClickListener(view -> ratingBar.setRating(3.5f));
+        btnSubmit.setOnClickListener(view -> {
+
+            ratingBar.getNumStars();
+            ratingBar.getRating();
+            showRatingSuccessfulToast();
+        });
+    }
+    @SuppressLint("SetTextI18n")
+    public void showRatingSuccessfulToast() {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast_success,
+                findViewById(R.id.toast_success));
+        TextView text = layout.findViewById(R.id.toast_text_success);
+        text.setText("Thank you for your rating");
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
     }
 }
